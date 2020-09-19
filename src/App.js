ea6c8ae5ecb1b06f2d2653d07dfb76d855c60e79
oguz <oguzkayaya@gmail.com>
2020-09-19 19:19:26 +0300

    wrap the all component with react-redux provider, pass the store

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Header from "./components/Header.js";
 import { BrowserRouter } from "react-router-dom";
 import { MovieProvider } from "./contexts/MovieContext.js";
 import { SearchProvider } from "./contexts/SearchContext.js";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <MovieProvider>
-          <SearchProvider>
-            <Header></Header>
-            <Movie></Movie>
-          </SearchProvider>
-        </MovieProvider>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <MovieProvider>
+            <SearchProvider>
+              <Header></Header>
+              <Movie></Movie>
+            </SearchProvider>
+          </MovieProvider>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 };
