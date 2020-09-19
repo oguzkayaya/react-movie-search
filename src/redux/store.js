f493e24afa49de8cef96ac3900780c32034b8764
oguz <oguzkayaya@gmail.com>
2020-09-19 19:48:35 +0300

    apply thunk middleware to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import movieListReducer from "./movieList/movieListReducer";
 
-const store = createStore(movieListReducer);
+const store = createStore(movieListReducer, applyMiddleware(thunk));
 
 export default store;
